a01362a99d49b85b9b784b981771ef1076f1d081
ASP <alu0100769609@ull.edu.es>
2015-11-10 20:45:49 +0000

    Creada expectativa de nodo

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -4,5 +4,13 @@ require 'spec_helper'
 require 'biblio_gem'
 
 describe BiblioGem::Node do
-    
+    describe "Existen nodos" do
+       before :each do
+           @nodo1 = BiblioGem::Node.new(:value => "1")
+       end
+       
+       it "El nodo es correcto" do
+           expect(@nodo1.to_s).to eq("1")
+       end
+   end
 end
