4bf38d61e685c35467df0034bbbb40c3e2b97014
ASP <alu0100769609@ull.edu.es>
2015-11-10 22:25:15 +0000

    Implementados métodos list.rb. Spec no se ejecuta bien

--- lib/biblio_gem/list.rb
+++ lib/biblio_gem/list.rb
@@ -1,5 +1,42 @@
 module BiblioGem
 	class List
-
+	    attr_accessor :head, :size
+	    def initialize(*dato)
+	        @head = nil
+	        @size = 0
+	        if (dato != nil)
+	            self.push(*dato)
+	        end
+	    end
+	   
+        def push(*dato)
+			if (@head != nil)
+				aux = @head
+				while (aux.next != nil) do
+					aux = aux.next
+				end 
+			end
+			
+			dato.each do |value|
+				if (@head == nil)
+					@head = Node.new(:value => value)
+					aux = @head
+				else
+					aux.next = Node.new(:value => value)
+					aux = aux.next
+				end
+				@size += 1
+			end
+        end
+        
+        def pop
+            if (@size == 0)
+                return false
+            end
+			aux = @head
+			@head = aux.next
+			@size -= 1
+			aux.value
+        end
 	end
 end
