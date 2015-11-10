aa15b98d46a4db14014a06cfcabb0f92ed1143db
ASP <alu0100769609@ull.edu.es>
2015-11-10 21:02:34 +0000

    Creado código para expectativa 1 de nodo, no se hace nada con next

--- lib/biblio_gem/node.rb
+++ lib/biblio_gem/node.rb
@@ -1,8 +1,12 @@
 module BiblioGem
     Node = Struct.new(:value, :next) do
+        attr_accessor :value, :next
+        def initialize(dato)
+            @value = dato[:value]
+        end
         
-        def initialize()
-
+        def to_s
+            @value.to_s
         end
     end
 end
