c7054566c397298c1ac9771727b2a21d1bdfb332
ASP <alu0100769609@ull.edu.es>
2015-11-16 18:26:54 +0000

    Creando la expectativa de un puntero al nodo anterior

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -25,4 +25,18 @@ describe BiblioGem::Node do
            expect(@nodo1.next.to_s).to eq("2")
        end
    end
+   
+   describe "Adición de un puntero hacia el anterior" do
+       before :each do
+           @nodo1 = BiblioGem::Node.new(:value => "1")
+           @nodo2 = BiblioGem::Node.new(:value => "2")
+           @nodo1.next = @nodo2
+           @nodo2.prev = @nodo1
+       end
+       
+       it "El nodo 2 apunta al anterior que es el nodo 1" do
+           expect(@nodo2.prev.to_s).to eq("1")
+       end
+   end
+   
 end
