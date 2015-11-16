ddcf95d54a534a242563e999fc31d9fc9dcc999c
ASP <alu0100769609@ull.edu.es>
2015-11-16 18:28:51 +0000

    Creado código que satisface expectativa del puntero al anterior

--- lib/biblio_gem/node.rb
+++ lib/biblio_gem/node.rb
@@ -1,9 +1,11 @@
+#Modificación prct8: Que se pueda recorrer en ambos sentidos no quiere decir que se puedan insertar por el otro lado..
 module BiblioGem
-    Node = Struct.new(:value, :next) do
-        attr_accessor :value, :next
+    Node = Struct.new(:value, :next, :prev) do
+        attr_accessor :value, :next, :prev
         def initialize(dato)
             @value = dato[:value]
             @next = dato[:next]
+            @prev = dato[:prev]
         end
         
         def to_s
